52505a4666b1b6b6a4dc0f47ca5d2f2044f36b46
seungyong88 <gosng2001@naver.com>
2021-08-25 17:24:40 +0900

    Update: app.js 조율 로직

--- src/App.js
+++ src/App.js
@@ -1,44 +1,79 @@
 // import { request } from "./api/api.js";
-// import Breadcrumb from "./components/Breadcrumb.js";
+import Breadcrumb from "./components/Breadcrumb.js";
 import Nodes from "./components/Nodes.js";
 
 // App.js
 function App($app) {
-  
-  const nodeItem = [
-    {
-      id: "5",
-      name: "2021/04",
-      type: 'DIRECTORY',
-      filePath: null,
-      parent: {
-        id: 1,
+  this.state = {
+    isRoot: false,
+    nodes: [
+      {
+        id: "5",
+        name: "2021/04",
+        type: 'DIRECTORY',
+        filePath: null,
+        parent: {
+          id: 1,
+        }
+      },
+      {
+        id: "19",
+        name: "물 마시는 사진",
+        type: 'FILE',
+        filePath: "/images/a2i.jpg",
+        parent: {
+          id: 1,
+        }
       }
-    },
-    {
-      id: "19",
-      name: "물 마시는 사진",
-      type: 'FILE',
-      filePath: "/images/a2i.jpg",
-      parent: {
-        id: 1,
+    ],
+    depth: [
+      {
+        id: "5",
+        name: "2021/04",
+        type: 'DIRECTORY',
+        filePath: null,
+        parent: {
+          id: 1,
+        }
+      },
+      {
+        id: "19",
+        name: "물 마시는 사진",
+        type: 'FILE',
+        filePath: "/images/a2i.jpg",
+        parent: {
+          id: 1,
+        }
       }
-    }
-  ]
+    ]
+  }
 
+  // nav 생성
+  const breadcrumb = new Breadcrumb({
+    $app,
+    initialState: this.state.depth
+  })
+
+  // 사진첩 생성 생성
   const nodes = new Nodes({
     $app, 
     initialState: {
-      node: nodeItem
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    },
+    onClick: (node) => {
+      if(node.type === "DIRECTORY"){
+        // DIRECTORY 처리
+        alert('DIRECTORY');
+      }else if(node.type === "FILE"){
+        // FILE 처리
+        alert('FILE');
+      }
     }
   })
 
-  // 이후 nodes를 갱신할 일이 있다면 nodes.setState를 호출
-  const nextState = {
-    node: nodeItem
-  }
-
-  nodes.setState(nextState);
+  breadcrumb.setState(this.state);
+  nodes.setState(this.state);
 }
 
 export default App;
